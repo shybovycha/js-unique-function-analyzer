24f59fbddd0b4efb917d1e95bc4dcbd7912477e8
Artem Shubovych <shybovycha@gmail.com>
2023-12-12 14:46:44 +1100

    Prettify code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# js-unique-functions
+# Unique functions analyzer
+
+## Overview
+
+This tool lets you see how many _unique_ functions you have in your JS source code and which of them have most duplicates.
+
+This comes useful when your modern JS application bundle consists of duplicates at `40%` or more, effectively wasting the client & server resources (on serving the duplicate bytes and parsing the same code on browser).
+
+## Usage
+
+Run `yarn analyze <your-input-js-file> <output-json-file>`.
+
+This will analyze your source JS file and write the analysis results to the provided output file.
--- index.js
+++ index.js
@@ -1,70 +1,65 @@
-const acorn = require('acorn');
-const walk = require('acorn-walk');
+const ts = require("typescript");
 const uglify = require('uglify-js');
 const fs = require('fs');
+const sha256 = require('sha256');
 
-// const code = `
-// (function () {
-//     const arbuz = (test) => {
-//         function apple(t) {
-//             function test () {
-//                 return 'ttt';
-//             }
-//             return t + 3;
-//         }
-//         const aa = 1;
-//         const b1 = () => 2;
-//         // comment
-//         return aa + b1() + apple(test);
-//     }
-//     return arbuz;
-//  })();
-// `;
+const sourceFilename = process.argv[2];
+const outputFilename = process.argv[3] || 'output.json';
 
-console.log('Reading code from file', process.argv[2])
+console.log(`Reading code from file ${sourceFilename}...`);
 
-const code = fs.readFileSync(process.argv[2]).toString();
-
-const functions = [];
-
-const parseOptions = {
-    ecmaVersion: 'latest',
-    sourceType: 'module,'
-};
+const code = fs.readFileSync(sourceFilename).toString();
 
 const minifyOptions = {
     expression: true,
 };
 
-walk.fullAncestor(acorn.parse(code, parseOptions), (node) => {
-    if (node.type === 'VariableDeclarator' && (node.init.type === 'ArrowFunctionExpression' || node.init.type === 'FunctionExpression')) {
-        const body = code.substring(node.init.start, node.init.end);
+console.log('Parsing file...');
+
+const root = ts.createSourceFile(
+    sourceFilename,
+    code,
+    ts.ScriptTarget.ESNext,
+    /*setParentNodes */ true
+);
+
+const functions = [];
+
+const parse = (node) => {
+    if (ts.isFunctionExpression(node) || ts.isArrowFunction(node) || ts.isFunctionDeclaration(node)) {
+        const body = code.substring(node.pos, node.end);
         const minCode = uglify.minify(body, minifyOptions);
 
-        // const outerBody = code.substring(node.start, node.end);
-        // const minOuterCode = uglify.minify(outerBody, minifyOptions);
-
         functions.push({
-            name: node.id.name,
-            // body,
-            code: minCode.error ? { error: minCode.error, source: body } : minCode.code,
-            // body,
-            // outerBody,
+            name: node.name && node.name.text,
+            code: minCode.code,
             // node,
-            // init: node.init,
-        });
-    } else
-    if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression' || node.type === 'ArrowFunctionExpression') {
-        const body = code.substring(node.start, node.end);
-        const minCode = uglify.minify(body);
-
-        functions.push({
-            name: node.id && node.id.name,
-            // body,
-            code: minCode.error ? { error: minCode.error, source: body } : minCode.code
         });
     }
-});
 
-console.log(functions.filter(({ name, code }) => !!name && !!code));
-// console.log(functions);
+    ts.forEachChild(node, child => {
+        parse(child);
+    });
+};
+
+parse(root);
+
+console.log('Analyzing file...');
+
+const uniqueFunctions = functions.map((fn) => ({ ...fn, hash: sha256(fn.code) })).reduce((acc, fn) => {
+    if (!acc[fn.hash]) {
+        acc[fn.hash] = [fn];
+    } else {
+        acc[fn.hash].push(fn);
+    }
+
+    return acc;
+}, {});
+
+const analysis = Object.entries(uniqueFunctions).map(([_, duplicates]) => ({ code: duplicates[0].code, duplicates: duplicates.length })).sort((a, b) => b.duplicates - a.duplicates);
+
+console.log(`Found ${functions.length} functions, ${Object.keys(uniqueFunctions).length} are unique (${Math.round((Object.keys(uniqueFunctions).length / functions.length) * 10000.0) / 100}%)`);
+
+fs.writeFileSync(outputFilename, JSON.stringify(analysis, null, '  '), 'utf-8');
+
+console.log(`Wrote analysis results to ${outputFilename}`);
