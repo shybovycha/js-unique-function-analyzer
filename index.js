6d8ce5f73da228b627e2bb005444a380c9931ad2
Artem Shubovych <shybovycha@gmail.com>
2023-12-13 14:46:54 +1100

    Add code duplication percent

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const ts = require("typescript");
+const ts = require('typescript');
 const uglify = require('uglify-js');
 const fs = require('fs');
 const sha256 = require('sha256');
+const { groupBy, sum } = require('lodash');
 
 const sourceFilename = process.argv[2];
 const outputFilename = process.argv[3] || 'output.json';
@@ -20,7 +21,7 @@ const root = ts.createSourceFile(
     sourceFilename,
     code,
     ts.ScriptTarget.ESNext,
-    /*setParentNodes */ true
+    /* setParentNodes */ true
 );
 
 const functions = [];
@@ -33,6 +34,8 @@ const parse = (node) => {
         functions.push({
             name: node.name && node.name.text,
             code: minCode.code,
+            pos: [node.pos, node.end],
+            length: node.end - node.pos,
             // node,
         });
     }
@@ -46,19 +49,23 @@ parse(root);
 
 console.log('Analyzing file...');
 
-const uniqueFunctions = functions.map((fn) => ({ ...fn, hash: sha256(fn.code) })).reduce((acc, fn) => {
-    if (!acc[fn.hash]) {
-        acc[fn.hash] = [fn];
-    } else {
-        acc[fn.hash].push(fn);
-    }
+const hashedFunctions = functions.map((fn) => ({ ...fn, hash: sha256(fn.code) }));
 
-    return acc;
-}, {});
+const uniqueFunctions = groupBy(hashedFunctions, 'hash');
 
-const analysis = Object.entries(uniqueFunctions).map(([_, duplicates]) => ({ code: duplicates[0].code, duplicates: duplicates.length })).sort((a, b) => b.duplicates - a.duplicates);
+const analysis = Object.entries(uniqueFunctions)
+    .map(([_, duplicates]) => ({
+        code: duplicates[0].code,
+        duplicates: duplicates.length,
+        length: sum(duplicates.sort((a, b) => a.length - b.length).slice(1).map(({ length }) => length)) // leave the shortest version aside and count the duplicate chars for the rest
+    }))
+    .sort((a, b) => b.duplicates - a.duplicates);
 
-console.log(`Found ${functions.length} functions, ${Object.keys(uniqueFunctions).length} are unique (${Math.round((Object.keys(uniqueFunctions).length / functions.length) * 10000.0) / 100}%)`);
+const duplicateLength = sum(Object.values(analysis).map(({ length }) => length));
+
+console.log(`Found ${functions.length} functions`);
+console.log(`${Object.keys(uniqueFunctions).length} are unique (${Math.round((Object.keys(uniqueFunctions).length / functions.length) * 10000.0) / 100}%)`);
+console.log(`Duplicates length: ${duplicateLength} out of ${code.length} bytes are duplicate code (${Math.round((duplicateLength / code.length) * 10000.0) / 100}%)`);
 
 fs.writeFileSync(outputFilename, JSON.stringify(analysis, null, '  '), 'utf-8');
 
